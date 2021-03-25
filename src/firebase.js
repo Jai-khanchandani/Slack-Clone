408d0f1721cd9e4659890a5b7f142dabcfd2264b
jai <khanchandanijai58@gmail.com>
2021-03-25 10:11:48 +0530

    Header Component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwNADNsqWAuAj0bW2dRDe6nOpuSoMEgws",
+    authDomain: "slack-clone-e2fed.firebaseapp.com",
+    projectId: "slack-clone-e2fed",
+    storageBucket: "slack-clone-e2fed.appspot.com",
+    messagingSenderId: "591433810150",
+    appId: "1:591433810150:web:432ca39bdb51607396bb27"
+  };
